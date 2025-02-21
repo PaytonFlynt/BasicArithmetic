ab23601f7de426ded2a8ebea6d770a5bc0e7c978
PaytonFlynt <pf0951950@otc.edu>
2025-02-20 19:57:08 -0600

    Add project files.

---
+++ BasicArithmetic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+ //Start the main function
+ int main() {
+
+        //Declare and initialize
+        int a = 10;
+        int b = 5;
+
+        //Arithmetic operations and display
+        std::cout << a << "+" << b << "=" << (a + b) << std::endl;
+        std::cout << a << "-" << b << "=" << (a - b) << std::endl;
+        std::cout << a << "*" << b << "=" << (a * b) << std::endl;
+        std::cout << a << "/" << b << "=" << (a / b) << std::endl;
+        std::cout << a << "%" << b << "=" << (a % b) << std::endl;
+
+        return 0;
+
+    }
